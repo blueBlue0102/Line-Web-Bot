39724e37cccfdac48cc249a7eb2c66281e34940e
Blue <ray1997860102@gmail.com>
2023-01-08 11:59:35 +0800

    將"登入"和"執行"分離，"執行"時不再登入

--- function.py
+++ function.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import time
-import urllib.parse
 import cv2
 import numpy
 import http.client
@@ -11,6 +10,7 @@ import sys
 import os
 
 lineCredentialPath = os.path.join('secrets', 'line-credential.json')
+
 
 class Client():
     def __init__(self):
@@ -22,7 +22,6 @@ class Client():
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'}
         self.session = requests.session()
         self.tempData = {}
-        self.loginWithQrCode()
         self.loadSession()
 
     def readJson(self, filename):
@@ -43,186 +42,36 @@ class Client():
         return
 
     def loadSession(self):
+        '''
+        讀取特定路徑下的 Line 憑證 \n
+        若憑證不存在，則報錯誤
+        若憑證存在，但是驗證後發現過期，報錯誤
+        '''
         credential = self.readJson(lineCredentialPath)
+
+        if not all(credKey in credential for credKey in ["authToken", "mid", "userId"]):
+            sys.exit('Certificate verification failed, please log in again.')
+
         self.session.cookies.set('ses', credential["authToken"])
-        if "mid" not in credential:
-            bots = self.getBots()
-            if bots == []:
-                print("[ ERROR ] No bot found on your account!!!")
-                sys.exit()
-            for n in range(len(bots)):
-                print(str(n+1)+'. '+bots[n]["name"])
-            choice = int(input("Please Select Account: "))-1
-            credential["mid"] = bots[choice]['botId']
-            credential["userId"] = bots[choice]['basicSearchId']
-            credential["name"] = bots[choice]['name']
-            self.writeJson(lineCredentialPath, credential)
         self.mid = credential["mid"]
         self.userId = credential["userId"]
+        checkLogin = json.loads(self.session.get(
+            url='https://chat.line.biz/api/v1/bots?noFilter=true&limit=1000',
+            headers=self.defaultHeaders,
+            data=None,
+            allow_redirects=True
+        ).text)
+        if 'code' in checkLogin:
+            sys.exit(f'Certificate verification failed, please log in again. Code: {checkLogin["code"]}')
+
         self.getCsrfToken()
-        print("[ NOTIF ] Success login...")
+
         # TODO: 應該要檢查 chatMode，欄位變成是 "current"
         # check = self.getChatMode()
         # if 'code' in check:
         #     print("[ ERROR ] Please set bot to chat mode!!!")
         #     sys.exit()
-
-    def loginWithQrCode(self):
-        credential = self.readJson(lineCredentialPath)
-        if "authToken" in credential:
-            self.session.cookies.set('ses', credential["authToken"])
-            checkLogin = json.loads(self.session.get(
-                url='https://chat.line.biz/api/v1/bots?noFilter=true&limit=1000',
-                headers=self.defaultHeaders,
-                data=None,
-                allow_redirects=True
-            ).text)
-            if 'code' not in checkLogin:
-                return
-        _csrf = self.session.get(
-            url='https://account.line.biz/login?redirectUri=https%3A%2F%2Fchat.line.biz%2F',
-            headers={
-                'Accept':
-                'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'en-US,en;q=0.9', 'Connection': 'keep-alive',
-                'Host': 'account.line.biz', 'Sec-Fetch-Dest': 'document', 'Sec-Fetch-Mode': 'navigate',
-                'Sec-Fetch-Site': 'none', 'Sec-Fetch-User': '?1', 'Upgrade-Insecure-Requests': '1',
-                'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-            data=None, allow_redirects=True).text.split('name="x-csrf" content="')[1].split('"')[0]
-
-        redir = self.session.post(
-            url='https://account.line.biz/login/line?type=login',
-            headers={
-                'Accept':
-                'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'en-US,en;q=0.9', 'Cache-Control':
-                'max-age=0', 'Connection': 'keep-alive', 'Host': 'account.line.biz',
-                'Origin': 'https://account.line.biz',
-                'Referer': 'https://account.line.biz/login?redirectUri=https%3A%2F%2Fchat.line.biz%2F',
-                'Sec-Fetch-Dest': 'document', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'same-origin',
-                'Sec-Fetch-User': '?1', 'Upgrade-Insecure-Requests': '1',
-                'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-            data={"_csrf": _csrf},
-            allow_redirects=False).headers["location"]
-
-        auth_url = self.session.get(
-            url=redir,
-            headers={
-                'accept':
-                'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate', 'accept-language': 'en-US,en;q=0.9', 'cache-control':
-                'max-age=0', 'referer': 'https://account.line.biz/', 'sec-fetch-dest': 'document',
-                'sec-fetch-mode': 'navigate', 'sec-fetch-site': 'cross-site', 'sec-fetch-user': '?1',
-                'upgrade-insecure-requests': '1',
-                'user-agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-            data=None, allow_redirects=False).headers["location"]
-
-        _state = urllib.parse.unquote(auth_url).split('&state=')[1].split('&')[0]
-        _loginState = urllib.parse.unquote(auth_url).split('loginState=')[1].split('&')[0]
-
-        redir = self.session.get(
-            url=auth_url,
-            headers={
-                'accept':
-                'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate', 'accept-language': 'en-US,en;q=0.9', 'cache-control':
-                'max-age=0', 'cookie': 'loginState=N8ovqhoa5fRKNP9PbHIzWi', 'referer': 'https://account.line.biz/',
-                'sec-fetch-dest': 'document', 'sec-fetch-mode': 'navigate', 'sec-fetch-site': 'cross-site',
-                'sec-fetch-user': '?1', 'upgrade-insecure-requests': '1',
-                'user-agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-            data=None, allow_redirects=True).url
-
-        qrCode = json.loads(
-            self.session.get(
-                url='https://access.line.me/qrlogin/v1/session?_=' + str(int(time.time())) + '&channelId=1576775644',
-                headers={'accept': 'application/json, text/plain, */*', 'accept-encoding': 'gzip, deflate',
-                         'accept-language': 'en-US,en;q=0.9', 'referer': redir, 'sec-fetch-dest': 'empty',
-                         'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin',
-                         'user-agent':
-                         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-                data=None, allow_redirects=True).text)["qrCodePath"]
-
-        downloadData = self.session.get(
-            url="https://access.line.me" + qrCode,
-            headers={'accept': 'image/avif,image/webp,image/apng,image/*,*/*;q=0.8', 'accept-encoding': 'gzip, deflate',
-                     'accept-language': 'en-US,en;q=0.9', 'referer': redir, 'sec-fetch-dest': 'image',
-                     'sec-fetch-mode': 'no-cors', 'sec-fetch-site': 'same-origin',
-                     'user-agent':
-                     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-            data=None, allow_redirects=True).content
-        print("Please use cellphone to scan qrcode for login")
-        print("PRESS ANY KEY BEFORE SCAN")
-        print("PRESS ANY KEY BEFORE SCAN")
-        print("PRESS ANY KEY BEFORE SCAN")
-        self.displayQrCode(downloadData)
-        print("Waiting for scan...")
-
-        pinCode = json.loads(
-            self.session.get(
-                url='https://access.line.me/qrlogin/v1/qr/wait?_=' + str(int(time.time())) + '&channelId=1576775644',
-                headers={'accept': 'application/json, text/plain, */*', 'accept-encoding': 'gzip, deflate',
-                         'accept-language': 'en-US,en;q=0.9', 'referer': redir, 'sec-fetch-dest': 'empty',
-                         'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin',
-                         'user-agent':
-                         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-                data=None, allow_redirects=True).text)["pinCode"]
-        print("Your pincode: "+pinCode)
-
-        pinCodeCallback = self.session.get(
-            url='https://access.line.me/qrlogin/v1/pin/wait?_=' + str(int(time.time())) + '&channelId=1576775644',
-            headers={'accept': 'application/json, text/plain, */*', 'accept-encoding': 'gzip, deflate',
-                     'accept-language': 'en-US,en;q=0.9', 'referer': redir, 'sec-fetch-dest': 'empty',
-                     'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin',
-                     'user-agent':
-                     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-            data=None, allow_redirects=True)
-        qrPinCert = pinCodeCallback.headers["set-cookie"].split('qrPinCert=')[1].split(";")[0]
-        cert = pinCodeCallback.headers["set-cookie"].split('cert=')[1].split(";")[0]
-
-        _csrf = self.session.cookies.get_dict()["X-SCGW-CSRF-Token"]
-        authLogin = self.session.get(
-            url='https://access.line.me/oauth2/v2.1/qr/authn?loginState=' + _loginState +
-            '&loginChannelId=1576775644&returnUri=%2Foauth2%2Fv2.1%2Fauthorize%2Fconsent%3Fscope%3Dprofile%26response_type%3Dcode%26state%3D'
-            + _state +
-            '%26redirect_uri%3Dhttps%253A%252F%252Faccount.line.biz%252Flogin%252Fline-callback%26client_id%3D1576775644&__csrf='
-            + _csrf,
-            headers={
-                'accept':
-                'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate', 'accept-language': 'en-US,en;q=0.9', 'referer': auth_url,
-                'sec-fetch-dest': 'document', 'sec-fetch-mode': 'navigate', 'sec-fetch-site': 'same-origin',
-                'upgrade-insecure-requests': '1',
-                'user-agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-            data=None, allow_redirects=False)
-        authLogin_redir = self.session.get(
-            url=authLogin.headers["location"],
-            headers={
-                'accept':
-                'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate', 'accept-language': 'en-US,en;q=0.9', 'referer': auth_url,
-                'sec-fetch-dest': 'document', 'sec-fetch-mode': 'navigate', 'sec-fetch-site': 'same-origin',
-                'upgrade-insecure-requests': '1',
-                'user-agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-            data=None, allow_redirects=False)
-        authLogin_result = self.session.get(
-            url=authLogin_redir.headers["location"],
-            headers={
-                'Accept':
-                'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'en-US,en;q=0.9', 'Connection': 'keep-alive',
-                'Host': 'account.line.biz', 'Referer': 'https://access.line.me/', 'Sec-Fetch-Dest': 'document',
-                'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'Upgrade-Insecure-Requests': '1',
-                'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'},
-            data=None, allow_redirects=False)
-        credential["authToken"] = authLogin_result.headers["Set-Cookie"].split(";")[0].replace("ses=", "")
-        self.writeJson(lineCredentialPath, credential)
+        print("[ NOTIF ] Success login...")
 
     def getCsrfToken(self):
         _csrf = json.loads(self.session.get(
